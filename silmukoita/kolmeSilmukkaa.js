d2b230cd72c78fbae79441d698dbb44ceb84ac90
Makutosi <korhoneneriko@gmail.com>
2021-03-29 01:57:17 +0300

    Aikaisemmin luodut tiedostot, jotka nyt viedään tähän repositioon/Eriko Korhonen/26.03.2021

---
+++ silmukoita/kolmeSilmukkaa.js
@@ -0,0 +1,14 @@
+for(var i=0; i<5; i ++){
+  console.log(i+ " . for-kierros");
+}
+var j = 0;//alustus
+while(j<5){//ehto
+  console.log(j+ ". while-kierros");
+  j++;//kasvatus
+}
+var k = 0;//alustus
+do{
+  console.log(k+ " .do-while-kierros");
+  k++;//kasvatus
+} while(k<5);//ehto
+//10.03.2021
