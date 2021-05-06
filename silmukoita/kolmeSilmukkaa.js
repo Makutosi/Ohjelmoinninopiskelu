541094eb82c53d402e05c502436de0d9b20cfac3
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-05-06 17:47:46 +0300

    Update kolmeSilmukkaa.js

--- silmukoita/kolmeSilmukkaa.js
+++ silmukoita/kolmeSilmukkaa.js
@@ -1,14 +1,25 @@
-for(var i=0; i<5; i ++){
+// kolme silmukkaa 10.3.2021
+// for-lause
+var i = 0;   // Muuttujan alustus
+for (i =0; i < 5; i ++)   // Ehto + määrä
+{
   console.log(i+ " . for-kierros");
 }
+
+// while-lause
 var j = 0;//alustus
-while(j<5){//ehto
+while (j < 5
+{
   console.log(j+ ". while-kierros");
   j++;//kasvatus
 }
+
+// do-while-lause
 var k = 0;//alustus
-do{
+do
+{
   console.log(k+ " .do-while-kierros");
   k++;//kasvatus
-} while(k<5);//ehto
-//10.03.2021
+} 
+while(k<5);//ehto
+
