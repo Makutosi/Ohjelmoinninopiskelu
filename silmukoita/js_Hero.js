03a05d53da9cdeaf846193b9989378689590da06
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-05-30 08:24:58 +0300

    Add files via upload
    Koodaushaasteita 1.viikko ja 2.viikko

---
+++ silmukoita/js_Hero.js
@@ -0,0 +1,27 @@
+// 1.viikko koodaushaaste 2  JS Hero/ Eriko Korhonen/ 17.3.2021
+// 1.tehtävä
+var firstname = " Lata";
+var etunimi = " Liisa";
+// merkkijonot ""    kirjaimet ''
+
+// 2.tehtävä
+// Geeta;
+
+// 3.tehtävä
+var flower;
+var tree;
+flower = 'rose';
+tree = 'maple';
+
+var flower = 'rose'; tree = 'maple'; // yhdelle riville
+// määritetään ja alustetaan
+
+// 4. tehtävä
+// x = 'Toe'    // arvo, joka tulee viimeisenä
+
+// 5.tehtävä  tietojen vaihto
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;  // väliaikainen arvo
+y = x;  // y = 'Laurel'
+x = z;  // x = ' Hardy'
