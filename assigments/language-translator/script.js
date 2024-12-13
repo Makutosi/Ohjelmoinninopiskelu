4b53685b9dedadb56aa808b0cd2685e4c4aa060c
eriko korhonen <korhoneneriko@gmail.com>
2024-12-13 20:24:54 +0200

    modify index and script files

--- assigments/language-translator/script.js
+++ assigments/language-translator/script.js
@@ -1,17 +1,17 @@
 /* Write a program that translates the text "Hello World" into the selected language 
  (English, Swedish, Spanish)*/
 
-document.getElementById('translate').addEventListener('click', () => {
-    const language = document.getElementById('language').value;
+document.getElementById("translate").addEventListener("click", function() {
+    const language = document.getElementById("language").value;
     let message;
 
-    if (language === 'en') {
-        message = 'Hello World';
-    } else if (language === 'sv') {
-        message = 'Hej Världen';
-    } else if (language === 'es') {
-        message = 'Hola Mundo';
+    if (language === "en") {
+        message = "Hello World";
+    } else if (language === "sv") {
+        message = "Hej Världen";
+    } else if (language === "es") {
+        message = "Hola Mundo";
     }
 
-    document.getElementById('output').textContent = message;
+    document.getElementById("output").textContent = message;
 });
