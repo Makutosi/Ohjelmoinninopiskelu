10e9fc28417d7accc0578ba11934e473de48800d
eriko korhonen <korhoneneriko@gmail.com>
2025-01-01 20:54:41 +0200

    create a new folder greeting

---
+++ koodaushaasteita/greeting/koodi.js
@@ -0,0 +1,19 @@
+function greeting() {
+    alert("Hello");
+    alert("World");
+}
+
+/* 
+ポップアップの使用例
+通知や警告 入力フォームにエラーがあった場合に、ユーザーにエラー内容を伝える
+alert("Please fill out all required fields.");
+
+確認ダイアログ 重要な操作（例: データ削除、ページの移動）を実行する前に、ユーザーに確認を求め
+if (confirm("Are you sure you want to delete this item?")) {
+    // ユーザーが OK をクリックした場合
+}
+
+情報の提示 簡単なメッセージや指示をユーザーに伝える。
+alert("Welcome to our website!");
+
+*/
