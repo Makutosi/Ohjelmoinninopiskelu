3a3141c486b8937f8b3d2e71568292fa81619bf9
eriko korhonen <korhoneneriko@gmail.com>
2025-02-12 22:16:25 +0200

    add different version of code and code esplanations

--- assigments/hip-heijaa/script.js
+++ assigments/hip-heijaa/script.js
@@ -42,7 +42,7 @@ function printNumbers() {
         } else if (i % 5 === 0) {
             result += "heijaa<br>";
         } else {
-            result += i + "<br>";
+            result += i + "<br>"; //HTMLの 改行タグ 
         }
     }
 
@@ -52,7 +52,42 @@ function printNumbers() {
 // ボタンがクリックされたら printNumbers() を実行
 document.getElementById("showButton").addEventListener("click", printNumbers);
 */
+/* 
+関数定義: printNumbers() HTMLのボタンがクリックされたときに実行されます。
+var output = document.getElementById("output");
+var output は変数 output を定義
+document.getElementById("output") は、HTML ドキュメント内に存在する id="output" の要素を取得するためのメソッド
 
+var result = ""; 最初は空の文字列, 最終的に画面に表示する内容を格納するための変数
+output.innerHTML = result; ループが終わった後、result には"hip"、"heijaa"、"hipheijaa"、または数字がHTMLとして格納されます
+
+document.getElementById("showButton") 
+ボタン要素を取得し、そのボタンがクリックされたときに printNumbers 関数が実行されるように設定
+*/
+/* 
+function printNumbers() {
+    var output = document.getElementById("output");
+    var result = "";
+
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result += "hipheijaa";
+        } else if (i % 3 === 0) {
+            result += "hip";
+        } else if (i % 5 === 0) {
+            result += "heijaa";
+        } else {
+            result += i;
+        }
+
+        // 最後の要素でない場合、コンマを追加
+        if (i < 100) {
+            result += ", ";
+        }
+    }
+
+    output.innerHTML = result;
+}*/
 /* 
 「3と5の倍数を見つけよう！」みたいな学習ゲーム に応用
 ユーザーに 「この数字は何？」と質問して答えさせるクイズ 形式
